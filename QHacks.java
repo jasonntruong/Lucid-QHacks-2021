a88a0570682ce98f1f321ecf57fd801513499591
Jason Truong <71723813+jasont9923@users.noreply.github.com>
2021-01-22 19:44:39 -0500

    Removed Hi

--- QHacks.java
+++ QHacks.java
@@ -3,7 +3,6 @@ package QHacks2021;
     public class QHacks {
         public static void main(String[] args) {
             System.out.println("Hello");
-            System.out.println("HI");
     }
 
 }
