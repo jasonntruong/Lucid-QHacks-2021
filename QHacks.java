44e9d04af6fedafb5c6347d1e97c424e3b8e4ae9
Jason Truong <71723813+jasont9923@users.noreply.github.com>
2021-01-22 19:44:15 -0500

    Added Hi

--- QHacks.java
+++ QHacks.java
@@ -3,6 +3,7 @@ package QHacks2021;
     public class QHacks {
         public static void main(String[] args) {
             System.out.println("Hello");
+            System.out.println("HI");
     }
 
 }
