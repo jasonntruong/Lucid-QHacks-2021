ff12ae36eddb6d553fe715a9e1154dec7ce39c48
Jason Truong <71723813+jasont9923@users.noreply.github.com>
2021-01-23 04:36:25 -0500

    Added Camera module

--- Lucid/app/src/main/java/com/example/lucidmain/MainActivity.java
+++ Lucid/app/src/main/java/com/example/lucidmain/MainActivity.java
@@ -2,13 +2,59 @@ package com.example.lucidmain;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.KeyEvent;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageView logo, picture;
+
+    final int REQUEST_IMAGE_CAPTURE = 341;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        logo = findViewById(R.id.logo);
+        picture = findViewById(R.id.picture);
+        picture.setAlpha(0);
+
     }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if ((keyCode == KeyEvent.KEYCODE_VOLUME_DOWN)) {
+            picture.setAlpha(255);
+            logo.setAlpha(0);
+
+            takePic(picture);
+        }
+        return true;
+    }
+
+
+    public void takePic (View view) {
+        Intent takePicIntent = new Intent (MediaStore.ACTION_IMAGE_CAPTURE);
+
+        if (takePicIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePicIntent, REQUEST_IMAGE_CAPTURE);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode==RESULT_OK) {
+
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            picture.setImageBitmap(imageBitmap);
+        }
+
+    }
+
 }
