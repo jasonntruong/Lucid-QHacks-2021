e4dd8c59b40fafe1354788019dafa3b5865a4103
Jason Truong <71723813+jasont9923@users.noreply.github.com>
2021-01-23 22:57:16 -0500

    Added Vision API

--- Lucid/app/src/main/java/com/example/lucidmain/MainActivity.java
+++ Lucid/app/src/main/java/com/example/lucidmain/MainActivity.java
@@ -1,37 +1,161 @@
 package com.example.lucidmain;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.FileProvider;
 
 import android.content.Intent;
-import android.graphics.Bitmap;
+import android.net.Uri;
+
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
+import android.speech.tts.TextToSpeech;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
+import android.widget.ImageButton;
 import android.widget.ImageView;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
 
 public class MainActivity extends AppCompatActivity {
 
-    ImageView logo, picture;
     Animation bounce, blink;
 
-    final int REQUEST_IMAGE_CAPTURE = 341;
+    ImageView logo, picture;
+    ImageButton stopButton;
+
+    String operation;
+    Uri photoURI;
+
+    TextToSpeech tts;
+
+    visionAPI vis;
+
+    static final int REQUEST_IMAGE_CAPTURE = 2002;
+
+    public File createIMG() throws IOException {    //creates, saves, and returns an image file
+        deleteIMG(); //deletes all files before creating new image file so there's always only 1 image file saved on the device (saves space). No need to keep prev scanned images
+        String time = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imgName = "JPG_" + time + "_";
+
+        File dir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File img = File.createTempFile(imgName, ".jpg", dir);   //saves image file to Pictures directory
+
+        return img;
+    }
+
+    public void deleteIMG() {   //deletes all image files in the Pictures directory
+        File dir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File[] dirList = dir.listFiles();
+        for (File files : dirList) {
+            files.delete();
+        }
+    }
+
+    public void takePic () throws IOException { //takesPicture and saves them using createIMG() method above
+        Intent takePicIntent = new Intent (MediaStore.ACTION_IMAGE_CAPTURE);
+        File photo = null;
+        try{
+            photo = createIMG();    //calls createIMG() method that will create and return an image file
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        if (photo != null){ //if the photo exists, then we setup the Intent
+            photoURI = FileProvider.getUriForFile(this, "com.example.android.fileprovider", photo);
+            takePicIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);  //put in the photoURI data in the takePicIntent
+            startActivityForResult(takePicIntent, REQUEST_IMAGE_CAPTURE);   //start activity with the takePicIntent to actually take the picture
+        }
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) { //when activity started with the takePicIntent, it takes a picture
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode==RESULT_OK) {
+
+            picture.setImageURI(photoURI);  //set the URI to photoURI we got in takePic() method
+
+            //operation was set in onKeyDown method
+            if (operation.equals("text")){  //if volume up was pressed, we call processText method from visionAPI class
+                vis.processText(photoURI);
+            }
+
+            else if (operation.equals("object")){   //if volume down was pressed, we call processImage method from visionAPI class
+                vis.processImage(photoURI);
+            }
+
+            picture.setAlpha(255);  //after the processText or processImage method is finished, we must show the picture ImageView, hide the logo ImageView, and stop the blinking animation
+            logo.setAlpha(0);
+            logo.clearAnimation();
+        }
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) { //detects key presses
+
+        //Lucid uses volume buttons over on-screen buttons so users have a physical/tactile feedback of when they click buttons since accidental screen taps
+        //will leave blind users lost
+
+        if (keyCode == KeyEvent.KEYCODE_VOLUME_UP) {    //Volume up button is for text-reading (using Vision API's text recognition)
+            operation = "text";
+            tts.speak("Text mode", TextToSpeech.QUEUE_FLUSH, null); //TTS "Text mode" so the user knows exactly what mode they have just entered
+        }
+
+        else if ((keyCode == KeyEvent.KEYCODE_VOLUME_DOWN)) {   //Volume down button is for image-labelling (using Vision API's image-labelling)
+            operation = "object";
+            tts.speak("Object mode", TextToSpeech.QUEUE_FLUSH, null); //TTS "Object mode" so the user knows that they have entered this mode
+        }
+
+        try {
+            takePic();  //takePic method called so user can take image of what they want
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return true;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        logo = findViewById(R.id.logo);
-        picture = findViewById(R.id.picture);
-        picture.setAlpha(0);
 
+        //Animations
         bounce = AnimationUtils.loadAnimation(this, R.anim.bounce);
         blink = AnimationUtils.loadAnimation(this, R.anim.blink_anim);
 
-        logo.startAnimation(bounce);
+        //ImageView
+        logo = (ImageView) findViewById(R.id.logo);
+        picture = (ImageView) findViewById(R.id.picture);
+        picture.setAlpha(0);    //hide picture ImageView at beginning
 
+        //ButtonView
+        stopButton = findViewById(R.id.stopButton);
+
+        //String
+        operation = "";
+
+        //Text-To-Speech
+        tts = new TextToSpeech(MainActivity.this, new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int status) {
+                if (status == TextToSpeech.SUCCESS){
+                    tts.setSpeechRate((float) 0.9); //lowered speech rate and language set to Canadian English
+                    tts.setLanguage(Locale.CANADA);
+                }
+            }
+        });
+
+        //Object from visionAPI class
+        vis = new visionAPI(getApplicationContext(), tts);
+
+        logo.startAnimation(bounce);
         bounce.setAnimationListener(new Animation.AnimationListener() {
             @Override
             public void onAnimationStart(Animation animation) {
@@ -40,7 +164,8 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void onAnimationEnd(Animation animation) {
-                logo.startAnimation(blink);
+                logo.startAnimation(blink); //after bounce animation ends, the logo will blink until picture is taken
+                tts.speak("Lucid is now ready to be your eyes", TextToSpeech.QUEUE_FLUSH, null); //Plays on app open so user knows that app is open
             }
 
             @Override
@@ -49,37 +174,11 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        stopButton.setOnClickListener(new View.OnClickListener() {  //User can stop TTS by simply clicking the screen
+            @Override
+            public void onClick(View v) {
+                tts.stop();
+            }
+        });
     }
-
-    @Override
-    public boolean onKeyDown(int keyCode, KeyEvent event) {
-        if ((keyCode == KeyEvent.KEYCODE_VOLUME_DOWN)) {
-            picture.setAlpha(255);
-            logo.setAlpha(0);
-
-            takePic(picture);
-        }
-        return true;
-    }
-
-
-    public void takePic (View view) {
-        Intent takePicIntent = new Intent (MediaStore.ACTION_IMAGE_CAPTURE);
-
-        if (takePicIntent.resolveActivity(getPackageManager()) != null) {
-            startActivityForResult(takePicIntent, REQUEST_IMAGE_CAPTURE);
-        }
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode==RESULT_OK) {
-
-            Bundle extras = data.getExtras();
-            Bitmap imageBitmap = (Bitmap) extras.get("data");
-            picture.setImageBitmap(imageBitmap);
-        }
-
-    }
-
 }
---
+++ Lucid/app/src/main/java/com/example/lucidmain/visionAPI.java
@@ -0,0 +1,98 @@
+package com.example.lucidmain;
+
+import android.content.Context;
+import android.net.Uri;
+import android.speech.tts.TextToSpeech;
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.mlkit.vision.common.InputImage;
+import com.google.mlkit.vision.label.ImageLabel;
+import com.google.mlkit.vision.label.ImageLabeler;
+import com.google.mlkit.vision.label.ImageLabeling;
+import com.google.mlkit.vision.label.defaults.ImageLabelerOptions;
+import com.google.mlkit.vision.text.Text;
+import com.google.mlkit.vision.text.TextRecognition;
+import com.google.mlkit.vision.text.TextRecognizer;
+
+import java.io.IOException;
+import java.util.List;
+
+public class visionAPI {    //Class that holds methods that uses Google cloud's Vision API
+
+    TextRecognizer tRec;
+    ImageLabeler labeler;
+    TextToSpeech mtts;
+
+    Context mContext;
+    protected visionAPI(Context context, TextToSpeech tts){ //object init takes context and tts as we need those 2 variables for the processText and processImage methods
+        mContext = context;
+        tRec = TextRecognition.getClient();
+        labeler = ImageLabeling.getClient(ImageLabelerOptions.DEFAULT_OPTIONS);
+        mtts = tts;
+    }
+
+    protected InputImage getImage(Uri photoURI){
+        InputImage image = null;
+        try {
+            image = InputImage.fromFilePath(mContext, photoURI);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return image;
+    }
+
+    protected void processText(Uri URI){
+        InputImage image = getImage(URI);
+
+        tRec.process(image).addOnSuccessListener(new OnSuccessListener<Text>() {
+            @Override
+            public void onSuccess(Text text) {
+                if (text.getText().length() > 0){
+                    mtts.speak(text.getText(), TextToSpeech.QUEUE_FLUSH, null);
+                }
+                else{
+                    mtts.speak("No text found", TextToSpeech.QUEUE_FLUSH, null);
+                }
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                mtts.speak("No text found", TextToSpeech.QUEUE_FLUSH, null);
+            }
+        });
+    }
+
+    protected void processImage(Uri URI) {
+        InputImage image = getImage(URI);
+
+        labeler.process(image).addOnSuccessListener(new OnSuccessListener<List<ImageLabel>>() {
+            @Override
+            public void onSuccess(List<ImageLabel> imageLabels) {
+                String allLabels = "";
+                for (ImageLabel labels : imageLabels) {
+                    Log.d("Label", String.valueOf(labels.getText() + " " + String.valueOf(labels.getConfidence())));
+                    if (labels.getConfidence() > 0.7) {
+                        allLabels += labels.getText() + "\n";
+                    }
+                }
+
+                if (allLabels.length() > 0){
+                    mtts.speak(allLabels, TextToSpeech.QUEUE_FLUSH, null);
+                }
+
+                else {
+                    mtts.speak("No objects found", TextToSpeech.QUEUE_FLUSH, null);
+                }
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                mtts.speak("No text found", TextToSpeech.QUEUE_FLUSH, null);
+            }
+        });
+    }
+}
